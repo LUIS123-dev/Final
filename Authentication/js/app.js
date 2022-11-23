0b18873e11091ab8ee69b2c8d1f39e6233fef284
Luis <luisfernandosantosbezerr@gmail.com>
2022-11-23 14:54:52 -0300

    atualizando projeto

---
+++ Authentication/js/app.js
@@ -0,0 +1,14 @@
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIygPdvNfHrQOWEAxrrXePn2ey9UYCG5g",
+  authDomain: "login-f90fb.firebaseapp.com",
+  projectId: "login-f90fb",
+  storageBucket: "login-f90fb.appspot.com",
+  messagingSenderId: "563923538964",
+  appId: "1:563923538964:web:1d74663c8791abad274301"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
